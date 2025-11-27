7029b799f57d17d852b090287088f335af588228
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-27 09:39:57 +0700

    Implement ContainerDataApp dengan OfflineRepositoriSiswa

--- app/src/main/java/com/example/myroom1p9/repositori/ContrainerApp.kt
+++ app/src/main/java/com/example/myroom1p9/repositori/ContrainerApp.kt
@@ -1,6 +1,19 @@
 package com.example.myroom1p9.repositori
 
+import android.content.Context
+import com.example.myroom1p9.room.DatabaseSiswa
+
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context) :
+    ContainerApp {
+
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
 }
 
