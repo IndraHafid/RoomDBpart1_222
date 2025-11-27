4a2791e6bb8e22a5da01da59091a28158d7459a5
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-27 09:35:43 +0700

    Implement OfflineRepositoriSiswa menggunakan SiswaDao

--- app/src/main/java/com/example/myroom1p9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroom1p9/repositori/RepositoriSiswa.kt
@@ -1,10 +1,20 @@
 package com.example.myroom1p9.repositori
 
 import com.example.myroom1p9.room.Siswa
+import com.example.myroom1p9.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+) : RepositoriSiswa {
+
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 }
