df2718bbbdc26ae06309d3e1f0069d3ac9e0f3a3
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-27 14:17:26 +0700

    Add Home navigation destination

---
+++ app/src/main/java/com/example/myroom1p9/view/route/DestinasiHome.kt
@@ -0,0 +1,7 @@
+package com.example.myroom1p9.view.route
+
+import com.example.myroom1p9.R
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Data Siswa"
+}
